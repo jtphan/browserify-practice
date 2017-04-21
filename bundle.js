f0e6a503588eb632fcd772e9b086954389f1eb85
Justin Phan <jpphan@ucdavis.edu>
2017-04-21 14:14:48 +0900

    Module exports and testing through main/html works. A little hacky, but alright

--- bundle.js
+++ bundle.js
@@ -41,14 +41,17 @@ var beep = require("./beep.js");
 
 console.log("Entering main js file");
 
-
 var response = document.getElementById("form1");
 
 response.addEventListener("submit", 
 	function() 
 	{
+		var str = document.getElementById("textEntry");
+		//console.log(str.value);
 		console.log("Submit confirmed!");
-		alert("BLAH BLAH BLAH");
+		var loud = beep.shout(str.value); 
+		console.log(loud);
+		alert(loud);
 	}); 
 },{"./beep.js":1}]},{},[2])(2)
 });
--- main.js
+++ main.js
@@ -2,12 +2,15 @@ var beep = require("./beep.js");
 
 console.log("Entering main js file");
 
-
 var response = document.getElementById("form1");
 
 response.addEventListener("submit", 
 	function() 
 	{
+		var str = document.getElementById("textEntry");
+		//console.log(str.value);
 		console.log("Submit confirmed!");
-		alert("BLAH BLAH BLAH");
+		var loud = beep.shout(str.value); 
+		console.log(loud);
+		alert(loud);
 	}); 
